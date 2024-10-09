a2cf8bd16783feb8dc563f69c6c83e57f0d54c76
Naoki Kato <naoki@bmoon.jp>
2024-10-10 07:27:01 +0900

    MakeCodeプロジェクトの初期ファイル

---
+++ lightsensor.ts
@@ -0,0 +1,13 @@
+/**
+ * Custom blocks
+ */
+//% weight=100 color=#0fbc11 icon="" block="光センサ v1.2"
+namespace IMLlightsensor {
+
+    //% block
+    //% block="明るさ %pin"
+    //% weight=100   
+    export function getLight(pin: AnalogPin): number {
+        return Math.round( pins.analogReadPin(pin)/1024*1000 )/10
+    }
+}
